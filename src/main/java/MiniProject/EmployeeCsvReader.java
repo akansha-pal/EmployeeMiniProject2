757c7e3a6ef3685e09c3dd5c25f315838f3fce9f
Akansha Pal <apal@spartaglobal.com>
2023-06-05 22:06:26 +0100

    updated testcase class

--- src/main/java/MiniProject/EmployeeCsvReader.java
+++ src/main/java/MiniProject/EmployeeCsvReader.java
@@ -8,8 +8,4 @@ public class EmployeeCsvReader {
                 var splitLines = line.split(",");
                 return new Employee(splitLines[0],splitLines[1], splitLines[2], splitLines[3],splitLines[4], splitLines[5]);
         }
-
-       /* public ArrayList<String> readFileLines(String fileName){
-                
-        }*/
 }
