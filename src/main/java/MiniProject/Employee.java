567b872cd16915c083a4e71b0c609cf52d4dc373
leilamohebi <leilamohebi25@gmail.com>
2023-06-05 16:43:50 +0100

    employee class

---
+++ src/main/java/MiniProject/Employee.java
@@ -0,0 +1,12 @@
+package MiniProject;
+
+public class Employee {
+    private String emp_no;
+    private String birth_date;
+    private String first_name;
+    private String last_name;
+    private String gender;
+    private String hire_date;
+
+
+}
