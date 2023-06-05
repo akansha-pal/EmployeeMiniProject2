3728f74649b5f4fac73e23c7b92322971108751c
leilamohebi <leilamohebi25@gmail.com>
2023-06-05 16:52:19 +0100

    employee constructor

--- src/main/java/MiniProject/Employee.java
+++ src/main/java/MiniProject/Employee.java
@@ -8,5 +8,12 @@ public class Employee {
     private String gender;
     private String hire_date;
 
-
+    public Employee(String emp_no, String birth_date, String first_name, String last_name, String gender, String hire_date) {
+        this.emp_no = emp_no;
+        this.birth_date = birth_date;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.gender = gender;
+        this.hire_date = hire_date;
+    }
 }
